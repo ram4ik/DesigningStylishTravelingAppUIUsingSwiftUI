2ab917b82ec8235a53546c237443ccab4579a7b7
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-07 23:04:52 +0200

    init

--- DesigningStylishTravelingAppUIUsingSwiftUI/ContentView.swift
+++ DesigningStylishTravelingAppUIUsingSwiftUI/ContentView.swift
@@ -10,12 +10,120 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            Home().tabItem {
+                Image(systemName: "house.fill")
+                    .font(.title)
+            }
+            Text("Activity").tabItem {
+                Image(systemName: "timelapse")
+                    .font(.title)
+            }
+            Text("Search").tabItem {
+                Image(systemName: "magnifyingglass.circle")
+                    .font(.title)
+            }
+            Text("Person").tabItem {
+                Image(systemName: "person.crop.circle")
+                    .font(.title)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            HStack {
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "scribble")
+                        .renderingMode(.original)
+                }
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "paperclip")
+                        .renderingMode(.original)
+                }
+            }.padding()
+            
+            Text("Find More")
+                .fontWeight(.heavy)
+                .font(.largeTitle)
+                .padding(.top, 15)
+            
+            HStack {
+                Button(action: {
+                    
+                }) {
+                    Text("Experiences")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Adventures")
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Activities")
+                        .foregroundColor(.gray)
+                }
+            }.padding(.top, 15)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 15) {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Button(action: {
+                            
+                        }) {
+                            Image("fishing")
+                                .renderingMode(.original)
+                        }
+                        Text("Fishing Time")
+                            .fontWeight(.heavy)
+                        
+                        HStack(spacing: 5) {
+                            Image(systemName: "map")
+                                .renderingMode(.original)
+                            Text("Vancouver, CA")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    VStack(alignment: .leading, spacing: 5) {
+                        Button(action: {
+                            
+                        }) {
+                            Image("camping")
+                                .renderingMode(.original)
+                        }
+                        Text("Forest Camping")
+                            .fontWeight(.heavy)
+                        
+                        HStack(spacing: 5) {
+                            Image(systemName: "map")
+                                .renderingMode(.original)
+                            Text("Kecamatan Klojan")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                
+                }
+            }
+        }.padding()
     }
 }
